8ca5d3acde9d70165b2f6d9ad333a6e5850694bb
Yoocodehash <lukaszkucharczyk263@gmail.com>
2024-05-15 10:04:04 -0400

    1

---
+++ MainMenu.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://LevelSelect.tscn")
+
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+
+
+func _on_quit_pressed():
+	get_tree().quit()
